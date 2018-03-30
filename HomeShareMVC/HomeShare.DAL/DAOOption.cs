aab807ce157713258f4b446ca8ef7cffa09a0d10
Thi Thu Trang Bui <buitrang9386@gmail.com>
2018-03-30 06:26:50 +0200

    DAOOption

---
+++ HomeShareMVC/HomeShare.DAL/DAOOption.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeShare.DAL
+{
+    public class DAOOption : DAO, IDAO<Options>
+    {
+        public Options create(Options obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Options read(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Options readByBien(int idBien)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Options> readAll()
+        {
+            SqlCommand cmd = connection.CreateCommand();
+            string query = "SELECT * FROM Options";
+            cmd.CommandText = query;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            List<Options> listOptions = new List<Options>();
+            foreach (DataRow row in dt.Rows)
+            {
+                Options unOption = new Options(
+                    (int)row["idOption"],
+                    row["Libelle"].ToString());
+                listOptions.Add(unOption);
+            }
+            return listOptions;
+        }    
+
+        public Options update(Options obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
